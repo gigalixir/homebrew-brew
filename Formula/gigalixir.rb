8747e307485096086fab59a24e2fd42c943736d9
Jesse <jeses@gigalixir.com>
2020-06-20 19:42:59 -0700

    v1.1.0

--- Formula/gigalixir.rb
+++ Formula/gigalixir.rb
@@ -2,32 +2,17 @@ class Gigalixir < Formula
   include Language::Python::Virtualenv
   desc "Gigalixir CLI"
   homepage "https://www.gigalixir.com"
-  url "https://github.com/gigalixir/gigalixir-cli/archive/v1.1.10.tar.gz"
-  sha256 "d8affe1e846833baa7ad973343c2b7006fb578854307038c05c3f716f72c0b9c"
+  url "https://github.com/gigalixir/gigalixir-cli/archive/1.1.0.tar.gz"
+  sha256 "8f97ac1239b817957840ff54ce67d76d9b1fdf5876dc60b33273a89cd93b153b"
 
   depends_on "python"
 
   # see NOTES.md for how to generate these resources
   # do *not* include gigalixir in this list!
 
-  resource "certifi" do
-    url "https://files.pythonhosted.org/packages/40/a7/ded59fa294b85ca206082306bba75469a38ea1c7d44ea7e1d64f5443d67a/certifi-2020.6.20.tar.gz"
-    sha256 "5930595817496dd21bb8dc35dad090f1c2cd0adfaf21204bf6732ca5d8ee34d3"
-  end
-
-  resource "chardet" do
-    url "https://files.pythonhosted.org/packages/fc/bb/a5768c230f9ddb03acc9ef3f0d4a3cf93462473795d18e9535498c8f929d/chardet-3.0.4.tar.gz"
-    sha256 "84ab92ed1c4d4f16916e05906b6b75a6c0fb5db821cc65e70cbd64a3e2a5eaae"
-  end
-
   resource "click" do
     url "https://files.pythonhosted.org/packages/95/d9/c3336b6b5711c3ab9d1d3a80f1a3e2afeb9d8c02a7166462f6cc96570897/click-6.7.tar.gz"
     sha256 "f15516df478d5a56180fbf80e68f206010e6d160fc39fa508b65e035fd75130b"
-  end
-
-  resource "idna" do
-    url "https://files.pythonhosted.org/packages/65/c4/80f97e9c9628f3cac9b98bfca0402ede54e0563b56482e3e6e45c43c4935/idna-2.7.tar.gz"
-    sha256 "684a38a6f903c1d71d6d5fac066b58d7768af4de2b832e426ec79c30daa94a16"
   end
 
   resource "Pygments" do
@@ -36,8 +21,8 @@ class Gigalixir < Formula
   end
 
   resource "requests" do
-    url "https://files.pythonhosted.org/packages/40/35/298c36d839547b50822985a2cf0611b3b978a5ab7a5af5562b8ebe3e1369/requests-2.20.1.tar.gz"
-    sha256 "ea881206e59f41dbd0bd445437d792e43906703fff75ca8ff43ccdb11f33f263"
+    url "https://files.pythonhosted.org/packages/16/09/37b69de7c924d318e51ece1c4ceb679bf93be9d05973bb30c35babd596e2/requests-2.13.0.tar.gz"
+    sha256 "5722cd09762faa01276230270ff16af7acf7c5c45d623868d9ba116f15791ce8"
   end
 
   resource "rollbar" do
@@ -53,11 +38,6 @@ class Gigalixir < Formula
   resource "stripe" do
     url "https://files.pythonhosted.org/packages/e3/67/5ab77380cec9874900f026ed21160e2e1a3b70b258c68740f700a8800025/stripe-1.51.0.tar.gz"
     sha256 "c9097e103a4c6c44fd019ee4e3fe70f529009bfc84764daf6e35a8f0fad91d21"
-  end
-
-  resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/8a/3c/1bb7ef6c435dea026f06ed9f3ba16aa93f9f4f5d3857a51a35dfa00882f1/urllib3-1.24.3.tar.gz"
-    sha256 "2393a695cd12afedd0dcb26fe5d50d0cf248e5a66f75dbd89a3d4eb333a61af4"
   end
 
   def install
